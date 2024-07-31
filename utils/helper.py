285a693b828c4ec740e14f9b52802c7da8eef507
Nitesh Gupta <nitesh.gupta@hirepro.in>
2024-07-31 16:40:46 +0530

    add get_tomorrow_date_utc function

--- utils/helper.py
+++ utils/helper.py
@@ -3,9 +3,14 @@ import datetime
 
 def get_datetime_utc():
     now = datetime.datetime.utcnow()
-
     # Format the date and time to the desired format
     formatted_date = now.isoformat(timespec='milliseconds') + 'Z'
-
     return formatted_date
+
+
+def get_tomorrow_date_utc():
+    today_utc = datetime.datetime.utcnow()
+    tomorrow_utc = today_utc + datetime.timedelta(days=1)
+    tomorrow_utc_str = tomorrow_utc.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
+    return tomorrow_utc_str
 
