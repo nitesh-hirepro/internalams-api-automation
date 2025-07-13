72b8cd7e25f16793d51cc97e6f6e201e3bcf98ef
Nitesh Gupta <nitesh.gupta@hirepro.in>
2025-07-13 13:54:40 +0530

    added timestamp and server name in resume extractor report

--- utils/output_path.py
+++ utils/output_path.py
@@ -1,9 +1,12 @@
 import os
-path = os.getenv("HOME")
+import datetime
+from hirepro_automation.enviroment import login_server
 
+path = os.getenv("HOME")
 generic_output_path = "%s/internalams_api_automation/reports/" % path
+now = datetime.datetime.now().strftime('%d-%m-%Y %I:%M:%S %p')
 
 output_paths = {
     'automation_report': generic_output_path + "impact/test_api_automation_report.xls",
-    'resume_extractor_report': generic_output_path + 'resume_extractor/resume_extraction_report.xlsx'
+    'resume_extractor_report': generic_output_path + f'resume_extractor/resume_extraction_report_{login_server}_{now}.xlsx'
 }
