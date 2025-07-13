7c4b67f732a97a8c025b144cff4e64a0f4e1ecd1
Nitesh Gupta <nitesh.gupta@hirepro.in>
2025-07-13 12:32:50 +0530

    more load login methods for different envs.

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -3,18 +3,32 @@ from utils.input_path import input_paths
 
 
 class DataLoader(ExcelRead):
+    login_data_file_path = input_paths['login_input_sheet']
+
     def __init__(self):
         super().__init__()
 
     def load_login_data(self):
-        self.excel_read(input_paths['login_input_sheet'], 0)
+        self.excel_read(DataLoader.login_data_file_path, 0)
         return self.complete_excel_data[0]
 
     def load_login_data_manager(self):
-        self.excel_read(input_paths['login_input_sheet'], 0)
+        self.excel_read(DataLoader.login_data_file_path, 0)
         return self.complete_excel_data[1]
 
     def load_login_data_admin(self):
-        self.excel_read(input_paths['login_input_sheet'], 0)
+        self.excel_read(DataLoader.login_data_file_path, 0)
         return self.complete_excel_data[2]
+
+    def load_login_data_recruiter_internalams(self):
+        self.excel_read(DataLoader.login_data_file_path, 1)
+        return self.complete_excel_data[0]
+
+    def load_login_data_recruiter_amsin(self):
+        self.excel_read(DataLoader.login_data_file_path, 2)
+        return self.complete_excel_data[0]
+
+    def load_login_data_recruiter_ams(self):
+        self.excel_read(DataLoader.login_data_file_path, 3)
+        return self.complete_excel_data[0]
 
